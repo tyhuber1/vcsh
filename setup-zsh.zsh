b08fa69123907e0e8c1302018d812a85eeea0ced
TyHuberD <tyhuber1@gmail.com>
2022-10-01 19:33:00 -0700

    adding zshrc

--- setup-zsh.zsh
+++ setup-zsh.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 set -euo pipefail
+D=$PWD
 cd ~
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -9,6 +10,6 @@ zsh -c 'source zsh-snap/install.zsh'
 # pk`0`
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # 
-echo "$(curl -fsSL https://gist.githubusercontent.com/tyhuber1/40d1935a8cf71c0a16a04b2724b37ccd/raw/85c794ec7d2ca3ec528b3cee1ea679ebb26eaaae/.zshrc)" > ~/.zshrc
+cp $D/.zshrc ~/.zshrc
 
 source ~/.zshrc
